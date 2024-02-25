1119df7f8bbd42cb118b3636270ea548401f8999
Pedro Fernandez <pefernandez1408@gmail.com>
2024-02-25 18:25:29 -0400

    ADD: Configuracion de la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 8080,
+    username: 'root',
+    password: '12345',
+    database: 'prueba',
+    autoLoadEntities: true,
+    synchronize: true
+  }),
+  UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
