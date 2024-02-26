97d80557398bfcf1e01b78249b05a5a6281d6cca
Pedro Fernandez <pefernandez1408@gmail.com>
2024-02-25 20:24:55 -0400

    ADD: Creacion del enpoint Post: /users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDTO } from './dto/user.dto';
+import { User } from './entity/user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private userService: UserService
+    ){}
+    
+    @Post('/register')
+    register(@Body() user: CreateUserDTO): Promise<User> {
+        return this.userService.create(user)
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './repository/user.repository';
+import { User } from './entity/user.entity';
+import { CreateUserDTO } from './dto/user.dto';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor( @InjectRepository(User) private userRepository: UserRepository ){}
+
+    async create(user: CreateUserDTO): Promise<User>{
+        const { name, password, role } = user
+        const hash = await bcrypt.hash(password, 10)
+        const newUser = this.userRepository.create({
+            name,
+            password: hash,
+            role
+        })
+        this.userRepository.save(newUser)
+        return newUser
+    }
+}
